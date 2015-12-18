1d404144f569556247ce620161e899ac2c861e32
Jason Shultz <jasshultz@gmail.com>
2015-12-17 17:38:34 -0700

    adding classes

--- app/classes/elevator.rb
+++ app/classes/elevator.rb
@@ -1,13 +1,16 @@
 class Elevator
 	include ActiveModel::Model
 
-	attr_accessor :current_floor, :passengers, :emtpy, :trips
+	attr_accessor :current_floor, :passengers, :emtpy, :trips, :door_status
 
-  def self.bar
-    puts 'class method'
-  end
-  
-  def baz
-    puts 'instance method'
-  end
+	before_save :default_values
+
+	def default_values
+		self.current_floor ||= 1
+	end
+
+	def self.bar
+		puts 'class method'
+	end
+
 end
--- app/classes/floor.rb
+++ app/classes/floor.rb
@@ -1,4 +1,12 @@
 class Floor
 	include ActiveModel::Model
 
+	attr_accessor :number_of_floors, :minimum_floor
+
+	before_save :default_values
+
+	def default_values
+		self.minimum_floor ||= 1
+	end
+
 end
